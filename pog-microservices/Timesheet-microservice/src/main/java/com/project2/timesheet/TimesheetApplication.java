7ec2ae066829928322da4cd73a4f4ac3ccba25ec
Ganghuan Huang <56037905+GH-H@users.noreply.github.com>
2022-02-10 08:42:57 -0500

    eureka config

--- pog-microservices/Timesheet-microservice/src/main/java/com/project2/timesheet/TimesheetApplication.java
+++ pog-microservices/Timesheet-microservice/src/main/java/com/project2/timesheet/TimesheetApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -13,6 +14,7 @@ import javax.sql.DataSource;
 
 //@EnableMongoRepositories('repository')
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableEurekaClient
 //@EnableDiscoveryClient
 public class TimesheetApplication {
 
